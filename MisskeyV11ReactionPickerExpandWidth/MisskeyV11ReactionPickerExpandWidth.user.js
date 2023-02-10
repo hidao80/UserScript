343e2cd94665d4be9ff735f114605197f1ce5e99
hidao80 <8155294+hidao80@users.noreply.github.com>
2023-02-11 05:21:50 +0900

    fix: The width of the text box is not 100%.

--- MisskeyV11ReactionPickerExpandWidth/MisskeyV11ReactionPickerExpandWidth.user.js
+++ MisskeyV11ReactionPickerExpandWidth/MisskeyV11ReactionPickerExpandWidth.user.js
@@ -3,7 +3,7 @@
 // @description Widen the width of the reaction picker in Misskey v11.
 // @match       https://misskey.dev/*
 // @author      hidao80
-// @version     1.1.2
+// @version     1.2
 // @namespace   https://github.com/hidao80/UserScript
 // @license     MIT
 // @icon        https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f44d.png
@@ -20,16 +20,26 @@
 //   https://github.com/twitter/twemoji/blob/master/LICENSE-GRAPHICS
 
 function expandWidth() {
-    const popover = document.querySelector('div[class*="popover isMobile"][data-v-0f5c3934=""]');
+    const popover = document.querySelector('div[class*="popover isMobile"][data-v-0f5c3934]');
     if (popover) {
-        popover.style.position = "fixed";
-        popover.style.top = "auto";
-        popover.style.bottom = "0";
-        popover.style.left = "0";
-        popover.style.transition = "0s";
+        const styles = {
+            "position": "fixed",
+            "top": "auto",
+            "bottom": "0",
+            "left": "0",
+            "transition": "none",
+            "animation": "none",
+            "transform": "none",
+        };
+        for (const prop in styles) {
+            popover.style[prop] = styles[prop];
+        }
         popover.querySelector('div[class="buttons"]').style.width = "100%";
-        popover.querySelector('div[class="text"]').style.width = "100%";
-        popover.querySelector('div[class="text"]').querySelector('input').style.width = "100%";
+        const text = popover.querySelector('div[class="text"]:not([type])');
+        if (text) {
+            text.style.width = "100%";
+            text.querySelector('input').style.width = "100%";
+        }
     }
 }
 
