4abe748c0fb9d7e0fcfd238229a70e772e202a56
hidao80 <8155294+hidao80@users.noreply.github.com>
2023-02-21 01:44:05 +0900

    improve: Make the background color 100% transparent

--- src/Misskey/MisskeyV11RemoteCustomEmojiMarker/MisskeyV11RemoteCustomEmojiMarker.user.js
+++ src/Misskey/MisskeyV11RemoteCustomEmojiMarker/MisskeyV11RemoteCustomEmojiMarker.user.js
@@ -3,7 +3,7 @@
 // @description Make remote custom emoji reactions prominent in Misskey v11.
 // @match       https://misskey.dev/*
 // @author      hidao80
-// @version     1.2
+// @version     1.3
 // @namespace   https://github.com/hidao80/UserScript/MisskeyV11RemoteCustomEmojiMarker
 // @license     MIT
 // @icon        https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f4e1.png
@@ -28,18 +28,25 @@
 const DEBUG = false;
 const SCRIPT_NAME = 'MisskeyV11RemoteCustomEmojiMarker';
 DEBUG && console.debug(`[${SCRIPT_NAME}]: script started.`);
+const TRANSPARENT = 'rgba(0,0,0,0)';
 
-const styles = [`
-.mk-reactions-viewer > span.reaction > img[title*="@"]:not(img[title$="@.:"]) {
-    opacity: 0.6;
+/**
+ * Make the background color 100% transparent
+ */
+function callback() {
+    setTimeout(() => {
+        const elems = document.querySelectorAll('.mk-reactions-viewer > span.reaction > img[title*="@"]:not([title$="@.:"]:not([transparent]))');
+        if (elems?.length) {
+            for (const elem of elems) {
+                const background = elem.parentElement;
+                if (background.style.backgroundColor != TRANSPARENT) {
+                    background.style.backgroundColor = TRANSPARENT;
+                    background.setAttribute('transparent', 'true');
+                }
+            }
+        }
+    }, 500);
 }
-`];
 
-// Style is a later winner, so send and add
-setTimeout(() => {
-    const usableSheet = [...document.styleSheets].slice(-1)[0];
-
-    for (let style of styles) {
-        usableSheet.insertRule(style, usableSheet.cssRules.length);
-    }
-}, 500);
+// Monitor whether emoji are drawn or not.
+(new MutationObserver(callback)).observe(document.body, { childList: true });
