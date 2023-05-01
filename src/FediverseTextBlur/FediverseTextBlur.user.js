4d34661f5273df34f250aab6b3499b6bb3307f89
hidao80 <8155294+hidao80@users.noreply.github.com>
2023-05-01 21:13:55 +0900

    fix: typo

--- src/FediverseTextBlur/FediverseTextBlur.user.js
+++ src/FediverseTextBlur/FediverseTextBlur.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Fediverse text blur
-// @nameja      Misskey & Mastodon テキストぼかし
+// @name:ja      Misskey & Mastodon テキストぼかし
 // @description Blur text in Misskey v11 and Mastodon.
 // @match       https://misskey.dev/*
 // @match       https://msky.work/*
@@ -9,7 +9,7 @@
 // @match       https://fedibird.com/*
 // @match       https://vivaldi.social.net/*
 // @author      hidao80
-// @version     1.0.1
+// @version     1.0.2
 // @namespace   https://github.com/hidao80/UserScript/FediverseTextBlur
 // @license     MIT
 // @icon        https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f4a7.png
