bb67b11e71144c96a18f78b2cbeadf60ecd07eb0
hidao80 <8155294+hidao80@users.noreply.github.com>
2023-05-01 13:16:56 +0900

    add: Browser back recommend disabler

---
+++ src/Others/BrowserBackRecommendDisabler/BrowserBackRecommendDisabler.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        Browser back recommend disabler
+// @name:ja     ブラウザバックレコメンド無効化
+// @description Prevent transitions to pages that prevent site leakage displayed in the browser back.
+// @match       https://*/*
+// @match       http://*/*
+// @author      hidao80
+// @version     1.0
+// @namespace   https://github.com/hidao80/UserScript/BrowserBackRecommendDisabler
+// @license     MIT
+// @icon        https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f6ab.png
+// @run-at      document-end
+// @grant       none
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js
+// @updateURL   https://github.com/hidao80/UserScript/raw/main/src/Others/BrowserBackRecommendDisabler/BrowserBackRecommendDisabler.user.js
+// @downloadURL https://github.com/hidao80/UserScript/raw/main/src/Others/BrowserBackRecommendDisabler/BrowserBackRecommendDisabler.user.js
+// ==/UserScript==
+
+// Twitter Emoji (Twemoji)
+// License
+//   Copyright 2019 Twitter, Inc and other contributors
+//   Graphics licensed under CC-BY 4.0: https://creativecommons.org/licenses/by/4.0/
+//   https://github.com/twitter/twemoji/blob/master/LICENSE-GRAPHICS
+
+'use strict';
+
+/** Constant variable */
+// When debugging: DEBUG = !false;
+const DEBUG = false;
+const SCRIPT_NAME = 'Browser back recommend disabler';
+/** Suppress debug printing unless in debug mode */
+const console = {};
+["log","debug","warn","info","error"].forEach(method => {
+    console[method] = DEBUG ? window.console[method] : function(){};
+});
+/** The script name is converted to a hexadecimal hash */
+const HASH = await (async (t=SCRIPT_NAME) => {const e=(new TextEncoder).encode(t),n=await crypto.subtle.digest("SHA-256",e);return Array.from(new Uint8Array(n)).map((t=>t.toString(16).padStart(2,"0"))).join("").slice(0,10)})();
+
+console.debug(`[${SCRIPT_NAME}]: HASH = ${HASH}`);
+console.debug(`[${SCRIPT_NAME}]: Script Loading...`);
+
+/** Individual script body */
+if ($._data($(window).get(0), "events")?.popstate?.length) {
+    console.debug(`[${SCRIPT_NAME}]: popstate off.`);
+    $(window).off("popstate");
+}
