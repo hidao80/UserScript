9246bd57b04ded8be2774a4d3b7bdfc7bacf284f
hidao80 <8155294+hidao80@users.noreply.github.com>
2023-05-07 05:57:41 +0900

    improve: Tap with three fingers to toggle enable/disable.

--- src/FediverseTextBlur/FediverseTextBlur.user.js
+++ src/FediverseTextBlur/FediverseTextBlur.user.js
@@ -9,11 +9,11 @@
 // @match       https://fedibird.com/*
 // @match       https://vivaldi.social.net/*
 // @author      hidao80
-// @version     1.0.3
+// @version     1.1.0
 // @namespace   https://github.com/hidao80/UserScript/FediverseTextBlur
 // @license     MIT
 // @icon        https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f4a7.png
-// @run-at      context-menu
+// @run-at      document-end
 // @grant       none
 // @updateURL   https://github.com/hidao80/UserScript/raw/main/src/FediverseTextBlur/FediverseTextBlur.user.js
 // @downloadURL https://github.com/hidao80/UserScript/raw/main/src/FediverseTextBlur/FediverseTextBlur.user.js
@@ -30,40 +30,58 @@
 /** Constant variable */
 // When debugging: DEBUG = !false;
 const DEBUG = !false;
-const SCRIPT_CLASS = 'us-hidao80-FediverseTextBlur';
-const SCRIPT_NAME = 'FediverseTextBlur';
-DEBUG && console.debug(`[${SCRIPT_NAME}]: script started.`);
+const SCRIPT_NAME = 'Fediverse Text Blur';
+/** Suppress debug printing unless in debug mode */
+const console = {};
+["log","debug","warn","info","error"].forEach(method => {
+    console[method] = DEBUG ? window.console[method] : function(){};
+});
+/** The script name is converted to a hexadecimal hash */
+const HASH = await (async (t=SCRIPT_NAME) => {const e=(new TextEncoder).encode(t),n=await crypto.subtle.digest("SHA-256",e);return Array.from(new Uint8Array(n)).map((t=>t.toString(16).padStart(2,"0"))).join("").slice(0,10)})();
+
+console.debug(`[${SCRIPT_NAME}]: HASH = ${HASH}`);
+console.debug(`[${SCRIPT_NAME}]: Script Loading...`);
+
 
 const styles = [
     `article .havbbuyv,
     article .status__content,
+    .reply-to .havbbuyv,
+    .reply-to .content,
     article .display-name {
         filter: blur(5px);
     }`,
 ];
 
-
-
-let indexes = JSON.parse(localStorage.getItem(SCRIPT_NAME + '_cssRules_indexes') || '[]');
-DEBUG && console.debug(`[${SCRIPT_NAME}]: init indexes:`);
-DEBUG && console.debug(indexes);
+let indexes = JSON.parse(localStorage.getItem(HASH + '_cssRules_indexes') || '[]');
+console.debug(`[${SCRIPT_NAME}]: init indexes:`);
+console.debug(indexes);
 
 // Style is a later winner, so send and add
 const usableSheet = [...document.styleSheets].slice(-1)[0];
 
-if (indexes.length > 0) {
-    for (const index of indexes) {
-        usableSheet.deleteRule(index);
+function blur() {
+    if (indexes.length > 0) {
+        for (const index of indexes) {
+            usableSheet.deleteRule(index);
+        }
+        localStorage.setItem(HASH + '_cssRules_indexes', '[]');
+        console.debug(`[${SCRIPT_NAME}]: styles deleted.`);
+    } else {
+        const index = usableSheet.cssRules.length;
+        for (let style of styles) {
+            indexes.push(index);
+            usableSheet.insertRule(style, index);
+        }
+        localStorage.setItem(HASH + '_cssRules_indexes', JSON.stringify(indexes));
+        console.debug(`[${SCRIPT_NAME}]: styles added.`);
+        console.debug(indexes);
     }
-    localStorage.setItem(SCRIPT_NAME + '_cssRules_indexes', '[]');
-    DEBUG && console.debug(`[${SCRIPT_NAME}]: styles deleted.`);
-} else {
-    const index = usableSheet.cssRules.length;
-    for (let style of styles) {
-        indexes.push(index);
-        usableSheet.insertRule(style, index);
-    }
-    localStorage.setItem(SCRIPT_NAME + '_cssRules_indexes', JSON.stringify(indexes));
-    DEBUG && console.debug(`[${SCRIPT_NAME}]: styles added.`);
-    DEBUG && console.debug(indexes);
 }
+
+// Tap with three fingers to toggle enable/disable.
+document.body.addEventListener("touchstart", e => {
+    if (e.touches.length == 3) {
+        blur();
+    }
+});
