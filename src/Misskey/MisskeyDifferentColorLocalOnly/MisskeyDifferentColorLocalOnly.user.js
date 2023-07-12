c5eb03c6ba92fc7115ed1e4094ee1fe5d0c76d0a
hidao <8155294+hidao80@users.noreply.github.com>
2023-07-13 07:18:31 +0900

    fix: Not only is it local, but it is also framed in the posting area.

--- src/Misskey/MisskeyDifferentColorLocalOnly/MisskeyDifferentColorLocalOnly.user.js
+++ src/Misskey/MisskeyDifferentColorLocalOnly/MisskeyDifferentColorLocalOnly.user.js
@@ -4,7 +4,7 @@
 // @description When the posting range is "local only" in Misskey, it should be a different color.
 // @match       https://misskey.dev/*
 // @author      hidao80
-// @version     1.0.3
+// @version     1.0.2
 // @namespace   https://github.com/hidao80/UserScript/MisskeyDifferentColorLocalOnly
 // @license     MIT
 // @icon        https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f3a8.png
@@ -25,7 +25,7 @@ const styles = [
     `.popover > div:nth-child(n+5) {
         color: var(--noteActionsReactionHover) !important;
     }`,
-    `.visibility > span, .visibility > span {
+    `div.local-only ~ footer > button.visibility > span {
         padding: 3px;
         border: solid 3px var(--noteActionsReactionHover);
         border-radius: 4px;
