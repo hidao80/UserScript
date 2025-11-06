91d93130fcbb6d785d8fa631ddb8a54d7a9e2b85
hidao80 <8155294+hidao80@users.noreply.github.com>
2025-11-06 19:37:16 +0900

    :bug: Fix crash when theme element is missing in localStorage

--- src/Misskey/MeiV11RenoteRemover/MeiV11RenoteRemover.user.js
+++ src/Misskey/MeiV11RenoteRemover/MeiV11RenoteRemover.user.js
@@ -32,7 +32,7 @@ DEBUG && console.debug(`[${SCRIPT_NAME}]: script started.`);
 // Trash icon SVG
 const buttonIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 1em; height: 1em; opacity: 1;">
     <g>
-        <path fill="${JSON.parse(localStorage.getItem('theme')).noteText}" d="M3 6h18v2H3V6zm2 3h14l-1.5 13h-11L5 9zm3 2v8h2v-8H8zm4 0v8h2v-8h-2zM9 4V2h6v2h5v2H4V4h5z"/>
+        <path fill="${JSON.parse(localStorage.getItem('theme'))?.noteText??'rgb(255, 255, 255)'}" d="M3 6h18v2H3V6zm2 3h14l-1.5 13h-11L5 9zm3 2v8h2v-8H8zm4 0v8h2v-8h-2zM9 4V2h6v2h5v2H4V4h5z"/>
     </g>
 </svg>`;
 
